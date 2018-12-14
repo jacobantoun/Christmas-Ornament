b75ae5510e31b43956ac1ce0fe045e32452325eb
Jacob A <jacob.antoun@gmail.com>
2018-12-14 16:42:44 -0500

    Add files via upload
    Atmel Studio 7 project for the ornament. Written in C

---
+++ ChristmasOrnament/ChristmasOrnament/main.c
@@ -0,0 +1,58 @@
+/*
+ * ChristmasOrnament.c
+ *
+ * Created: 12/11/2018 12:08:06 PM
+ * Author : jantoun
+ */ 
+
+#include <avr/io.h>
+#include <avr/delay.h>
+
+unsigned char ledPattern[4] = {
+	0b01010101,
+	0b10101010,
+	0b10011001,
+	0b01100110
+};
+uint16_t followPattern = 0b0000000000000000;
+
+int main(void)
+{
+    DDRA = 0xFF; // All Port A output
+	DDRB |= (1 << 0); // PB0 output
+	PORTA = 0;
+	PORTB &= ~(1 << 0);
+    while (1) 
+    {
+		
+		for(int i = 0; i<9; i++) {
+			followPattern = followPattern | (1 << i);
+			PORTA = followPattern;
+			PORTB = (followPattern >> 8);
+			_delay_ms(300);
+		}
+		for(int j = 9; j >= 0; j--) {
+			followPattern = followPattern &= ~(1 << j);
+			PORTA = followPattern;
+			PORTB = (followPattern >> 8);
+			_delay_ms(300);
+		}
+		
+		PORTA = ledPattern[0];
+		PORTB |= (1 << PORTB0);
+		_delay_ms(1000);
+		PORTA = ledPattern[1];
+		PORTB &= ~(1 << PORTB0);	
+		_delay_ms(1000);
+		PORTA = ledPattern[2];
+		PORTB |= (1 << PORTB0);
+		_delay_ms(1000);
+		PORTA = ledPattern[3];
+		PORTB &= ~(1 << PORTB0);
+		_delay_ms(1000);
+			
+    }
+}
+
+
+
